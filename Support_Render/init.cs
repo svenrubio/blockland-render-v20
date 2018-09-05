svenrubio/blockland-render-v20

commit 4f711f9342bc7b87e619a8f8aaf9b31ca1c0ed38

Spawnrate re-balance (2)
Second attempt at re-balancing the spawnrate. This also affects ambient sounds.

Support_Render/init.cs
@@ -25,7 +25,7 @@ if(isFunction("RTB_registerPref"))
 
 	//RTB_registerPref("Difficulty", "Render", "$Pref::Server::RenderDifficulty", "list Passive 0 Easy 25 Normal 100 Hard 400 Insane 1600", "Support_Render", $Pref::Server::RenderDifficulty, 0, 0);
 	RTB_registerPref("Mode", "Render", "$Pref::Server::RenderDamageType", "list Normal 0 Damage 1 Tag 2 Haunt 3", "Support_Render", 0, 0, 0);
-	RTB_registerPref("Spawn Rate", "Render", "$Pref::Server::RenderSpawnRate", "list Disabled 0 Low 2 Below_Normal 3 Normal 4 Above_Normal 5 High 6", "Support_Render", 4, 0, 0);
+	RTB_registerPref("Spawn Rate", "Render", "$Pref::Server::RenderSpawnRate", "list Disabled 0 Low 1 Below_Normal 5 Normal 7 Above_Normal 12 High 14", "Support_Render", 4, 0, 0);
 	RTB_registerPref("Shrine Range", "Render", "$Pref::Server::RenderShrineRange", "list 64x 28 48x 20 32x 12 16x 4 Disabled -1", "Support_Render", 28, 0, 0);
 	RTB_registerPref("Shrines admin only", "Render", "$Pref::Server::RenderAdminShrines", "bool", "Support_Render", 0, 0, 0);
 	RTB_registerPref("Affect bricks and lights", "Render", "$Pref::Server::RenderAllowBrickEffects", "bool", "Support_Render", 1, 0, 0);
@@ -47,6 +47,14 @@ else
 	$Pref::Server::RenderSpawnRate = 4;
 	$Pref::Server::RenderShrineRange = 28;
 	$Pref::Server::RenderAllowBrickEffects = 1;
+}
+
+// Spawnrate pref reverse compatibility
+// This prevents the pref from working incorrectly if updating from an old version.
+%rate = $Pref::Server::RenderSpawnRate;
+if(%rate == 2 || %rate == 3 || %rate == 4 || %rate == 6)
+{
+	$Pref::Server::RenderSpawnRate = 7;
 }
 
 // Extra load check; these are the things that REALLY shouldn't run twice.

Support_Render/render.cs
@@ -491,7 +491,7 @@ function Render_SunlightCheck()
 }
 
 ////// # Target picking function
-//This will determine the targets for the attacker and start the spawning code
+// This will determine the targets for the attacker and start the spawning code
 function Render_Spawn_Loop()
 {
 	if(isEventPending($Render::LoopSpawner))
@@ -510,9 +510,8 @@ function Render_Spawn_Loop()
 	if(!%skipSpawn)
 	{
 		// Play ambient sound effects
-
 		if(!$Pref::Server::RenderDisableAmbientSounds && !Render_SunlightCheck())
-			if(getRandom(1,12) <= $Pref::Server::RenderSpawnRate) // Bleh
+			if(getRandom(1,24) <= $Pref::Server::RenderSpawnRate) // Bleh
 				serverPlay2D("RenderAmb" @ getRandom(1,3));
 
 		// Render uses a 'group' spawning system to choose which players to target. This works by choosing between areas rather than individual players.
@@ -559,8 +558,8 @@ function Render_Spawn_Loop()
 			//echo("RENDER: Average spawnrate is " @ %avgSpawnrate);
 
 			// Now, we choose if we want to spawn for this group.
-			// TODO: Re-balance this
-			%random = getRandom(1,6);
+			%random = getRandom(1,18);
+//			talk(%random SPC %avgSpawnrate);
 			if(%random <= %avgSpawnrate)
 			{
 				// If yes, we'll pick a random player in the group to start with.

commit a831cdc61f45fb2503ceb3c07ab1817f84d647b3

Fix default spawnrate
Fixed the spawnrate not working correctly without a preferences add-on installed or when first installing the add-on. Caused by: 4f711f9

Support_Render/init.cs
@@ -25,7 +25,7 @@ if(isFunction("RTB_registerPref"))
 
 	//RTB_registerPref("Difficulty", "Render", "$Pref::Server::RenderDifficulty", "list Passive 0 Easy 25 Normal 100 Hard 400 Insane 1600", "Support_Render", $Pref::Server::RenderDifficulty, 0, 0);
 	RTB_registerPref("Mode", "Render", "$Pref::Server::RenderDamageType", "list Normal 0 Damage 1 Tag 2 Haunt 3", "Support_Render", 0, 0, 0);
-	RTB_registerPref("Spawn Rate", "Render", "$Pref::Server::RenderSpawnRate", "list Disabled 0 Low 1 Below_Normal 5 Normal 7 Above_Normal 12 High 14", "Support_Render", 4, 0, 0);
+	RTB_registerPref("Spawn Rate", "Render", "$Pref::Server::RenderSpawnRate", "list Disabled 0 Low 1 Below_Normal 5 Normal 7 Above_Normal 12 High 14", "Support_Render", 7, 0, 0);
 	RTB_registerPref("Shrine Range", "Render", "$Pref::Server::RenderShrineRange", "list 64x 28 48x 20 32x 12 16x 4 Disabled -1", "Support_Render", 28, 0, 0);
 	RTB_registerPref("Shrines admin only", "Render", "$Pref::Server::RenderAdminShrines", "bool", "Support_Render", 0, 0, 0);
 	RTB_registerPref("Affect bricks and lights", "Render", "$Pref::Server::RenderAllowBrickEffects", "bool", "Support_Render", 1, 0, 0);
@@ -44,7 +44,7 @@ else
 {
 	//$Pref::Server::RenderDifficulty = 100;
 	$Pref::Server::RenderDamageType = 0;
-	$Pref::Server::RenderSpawnRate = 4;
+	$Pref::Server::RenderSpawnRate = 7;
 	$Pref::Server::RenderShrineRange = 28;
 	$Pref::Server::RenderAllowBrickEffects = 1;
 }