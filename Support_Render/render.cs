4777b4c7325633e353e608ca62311c167ff3ae99
Lake <contact@lakeys.net>
2018-09-23 16:03:41 -0400

    Fix chance

--- Support_Render/render.cs
+++ Support_Render/render.cs
@@ -118,7 +118,7 @@ function Render_CreateBot(%pos,%client)
 	if(getRandom(1,384) == 1) {
 		%render.type = "ts";
 	}
-	else if(getRandom(1,1) == 1) {
+	else if(getRandom(1,20) == 1) {
 		%render.type = "g";
 	}
 	else {
