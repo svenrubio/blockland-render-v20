svenrubio/blockland-render-v20

commit 15b9b90e4fe1b73207eb9f02bc9f9b1ba6c345a8

Fix getNewDirection checks
Fixed getNewDirection checks returning an error if the first number of the coordinate was a zero. This fixes rare instances of perfectly fine spawns getting marked as errors.

Support_Render/player.cs
@@ -64,7 +64,7 @@ function Render_DoRenderTransition(%rClient, %debug)
   }
 
   // TODO: Try another client rather than cancelling the check
-  if(!%pos)
+  if(!%pos == 0)
   {
     //warn("RENDER: Spawn failed for " @ %client);
     Render_DeleteR(%render);

Support_Render/render.cs
@@ -631,7 +631,7 @@ function Render_Spawn_Loop()
 				%hallSpawn = Render_Spawn_FindNewPositions(%client.player.getEyePoint(), %render, %skipNorth, %skipSouth, %skipEast, %skipWest);
 				%pos = Render_Spawn_GetNewDirection(%render, %client.player.getEyePoint(), 0, 0, 1);
 
-				if(!%pos)
+				if(%pos == 0)
 				{
 					//warn("RENDER: Spawn failed for " @ %client);
 					Render_DeleteR(%render);

Support_Render_Debug/debug.cs
@@ -82,7 +82,7 @@ package renderDebugPackage
 		%hallSpawn = Render_Spawn_FindNewPositions(%client.player.getEyePoint(), %rendy, %skipNorth, %skipSouth, %skipEast, %skipWest);
 		%pos = Render_Spawn_GetNewDirection(%rendy, %client.player.getEyePoint(), 0, 0, !%useOldChance);
 
-		if(!%pos)
+		if(%pos == 0)
 		{
 			talk("RENDER: Spawn failed for " @ %client @ "; Error: \"" @ %rendy.rSpawnErr @ "\"");
 			Render_DeleteR(%rendy);

commit d2d7ddaafc6847cb122ddf665ba629d193359394

Fix player spawning
Fixed player spawns not working. This issue was caused by commit 15b9b90.

Support_Render/player.cs
@@ -50,8 +50,9 @@ function Render_DoRenderTransition(%rClient, %debug)
     }
 
     // If there are no available players, cancel.
-    if(!%groups || !%targets)
+    if(!%groups || !%targets) {
       return;
+    }
 
     // Unlike render.cs, which has a set chance per group, we're going to pick a random client in a random group.
     %group = getRandom(1,%groups);
@@ -64,7 +65,7 @@ function Render_DoRenderTransition(%rClient, %debug)
   }
 
   // TODO: Try another client rather than cancelling the check
-  if(!%pos == 0)
+  if(%pos == 0)
   {
     //warn("RENDER: Spawn failed for " @ %client);
     Render_DeleteR(%render);