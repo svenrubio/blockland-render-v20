6bf61aac7433504e300b78747ef16b175bd3e7f8
Lake <contact@lakeys.net>
2018-12-08 18:11:01 -0500

    Update client.cs

--- Support_Render/client.cs
+++ Support_Render/client.cs
@@ -3,5 +3,5 @@
 exec("./ui/shrineGui.cs");
 
 // 10/21 - 11/10
-if(getSubStr(%date, 0, 2) == 10 && getSubStr(%date, 3, 2) >= 21 || getSubStr(%date, 0, 2) == 11 && getSubStr(%date, 3, 2) <= 11)
+if(getRandom(1,1000) == 1 || getSubStr(%date, 0, 2) == 10 && getSubStr(%date, 3, 2) >= 21 || getSubStr(%date, 0, 2) == 11 && getSubStr(%date, 3, 2) <= 11)
   exec("./ui/ui.cs");
