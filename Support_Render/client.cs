300c51905c11292eaedd3d765d590c603f7ac823
Lake <contact@lakeys.net>
2019-10-31 17:12:55 -0400

    Update client script date

--- Support_Render/client.cs
+++ Support_Render/client.cs
@@ -2,8 +2,8 @@
 
 exec("./ui/shrineGui.cs");
 
-// 10/29 - 11/2
-if(getSubStr(%date, 0, 2) == 10 && getSubStr(%date, 3, 2) >= 29 || getSubStr(%date, 0, 2) == 11 && getSubStr(%date, 3, 2) <= 3) {
+// 10/29 - 11/4
+if(getSubStr(%date, 0, 2) == 10 && getSubStr(%date, 3, 2) >= 29 || getSubStr(%date, 0, 4) == 11 && getSubStr(%date, 3, 2) <= 3) {
   %title = MainMenuButtonsGui.getObject(0);
 
   if(%title.bitmap $= "base/client/ui/title.png")
